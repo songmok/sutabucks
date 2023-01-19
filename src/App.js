f2e1915538472962503c61eb4d7ad872b85861e5
Yenhj <yen.greact@gmail.com>
2023-01-19 16:34:58 +0900

    d

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Reset } from "styled-reset";
+// Routes
+import Header from "./components/header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "pages/menu/Menu";
+import NotFound from "./pages/NotFound";
+// api
+import instance from "./api/axios";
+import requests from "./api/request";
+import Login from "pages/member/Login";
+import SignUp from "pages/member/SignUp";
+import IdFind from "pages/member/IdFind";
+import PwFind from "pages/member/PwFind";
+import Menudetail from "pages/menu/Menudetail";
+import Cart from "pages/cart/Cart";
+import Order from "pages/cart/Order";
+import IdResult from "pages/member/IdResult";
+import PwResult from "pages/member/PwResult";
+import MyPage from "pages/member/MyPage";
+
+const App = () => {
+  return (
+    <Router>
+      <Reset />
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/idfind" element={<IdFind />} />
+        <Route path="/idresult" element={<IdResult />} />
+        <Route path="/pwfind" element={<PwFind />} />
+        <Route path="/pwresult" element={<PwResult />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menudetail" element={<Menudetail />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,62 @@
+import React from "react";
+import cartImg from "asset/images/cart.png";
+import HeaderCss from "style/HeaderCss";
+import { Link } from "react-router-dom";
+import logo1 from "asset/images/logo1.png";
+
+const Header = () => {
+  return (
+    <>
+      <HeaderCss>
+        <div className="header-wrap">
+          <Link to="/" className="logo">
+            <img src={logo1} alt="logo" />
+          </Link>
+          <div className="header-center">
+            <h1 className="title">SUTABUCKS</h1>
+            <nav>
+              <ul className="gnb">
+                <li className="depth1">
+                  <Link to="/">새소식</Link>
+                </li>
+                <li className="depth1">
+                  <Link to="/menu">매뉴보기</Link>
+                </li>
+                <li className="depth1">
+                  <Link to="/delivery">매장선택</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="header-right">
+            <div className="header-right-top">
+              <div className="cart">
+                <Link to="/cart" className="cart-link">
+                  <img src={cartImg} alt="cart" />
+                  <span>장바구니</span>
+                </Link>
+              </div>
+              <ul className="member">
+                <li className="member-list">
+                  <Link to="/login">로그인</Link>
+                </li>
+                <li className="member-list">
+                  <Link to="/signup">회원가입</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="header-right-bottom">
+              <div className="order">
+                <Link to="/delivery">
+                  <span>주문하기</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </HeaderCss>
+    </>
+  );
+};
+
+export default Header;
