34c2d1adb527e9b411a9757768fe6a13c45c9884
songmok <songmokseed@gmail.com>
2023-01-19 16:20:02 +0900

    Merge branch 'master' of https://github.com/songmok/sutabucks
    # Conflicts:
    #	src/components/Header/CartCss.js
    #	src/components/Header/MenudetailCss.js
    #	src/components/Header/OrderModalCss.js

---
+++ src/components/Header/MenudetailCss.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+const MenudetailCss = styled.section`
+  .detail-title {
+    border-top: 1px solid grey;
+    border-bottom: 1px solid grey;
+  }
+`;
+
+export default MenudetailCss;
