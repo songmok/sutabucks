f2756a3f04c5973fd718526d799c283bbf98520f
songmok <118712217+songmok@users.noreply.github.com>
2023-02-03 14:22:08 +0900

    수정

--- src/components/common/header/headerRt/orderbtn/HeaderOrderBtn.js
+++ src/components/common/header/headerRt/orderbtn/HeaderOrderBtn.js
@@ -3,15 +3,7 @@ import { Link } from "react-router-dom";
 const HeaderOrderBtn = ({ user }) => {
   return (
     <>
-      {user.miBusinessNum === "" ? (
-        <div className="header-right-bottom">
-          <div className="order">
-            <Link to="/maps">
-              <span>주문메뉴</span>
-            </Link>
-          </div>
-        </div>
-      ) : (
+      {user.miGroup === 2 ? (
         <div className="header-right-bottom">
           <div className="order">
             <Link to="/order">
@@ -19,9 +11,16 @@ const HeaderOrderBtn = ({ user }) => {
             </Link>
           </div>
         </div>
+      ) : (
+        <div className="header-right-bottom">
+          <div className="order">
+            <Link to="/order">
+              <span>주문메뉴</span>
+            </Link>
+          </div>
+        </div>
       )}
     </>
   );
 };
-
 export default HeaderOrderBtn;
